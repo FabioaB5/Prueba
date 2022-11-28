73df6fea8d3eb4d67918d8a8142419a5e2be42df
FabioaB5 <andresbarreiroa@gmail.com>
2022-11-28 14:36:38 -0500

    actualizar

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask 
-from pymongo import MongoClient
-import json 
-import certifi
+from flask_pymongo import flask_pymongo
+import flask 
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+mongodb_client = PyMongo(app, uri="mongodb://localhost:27017/todo_db")
+db = mongodb_client.db
 
-@app.route('/')
-def inicio():
-    return "Hola Prueba"
+@app.route("/add_one")
+def add_one():
+    db.todos.insert_one({'title': "todo title", 'body': "todo body"})
+    return flask.jsonify(message="success")
